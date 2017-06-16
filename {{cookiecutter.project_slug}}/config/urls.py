e253ae1dbd1ba4ea21d12b43b4ad1dc1342b58e5
silau2005 <steve.cklau@gmail.com>
2017-06-16 16:01:06 +0800

    Update urls.py

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
+from wagtail.contrib.wagtailsitemaps.views import sitemap
 
 urlpatterns = [
     url(r'^home/', TemplateView.as_view(template_name='pages/home.html'), name='home'),
